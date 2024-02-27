ea69774d3eab551d8d1a93c7c495f6f0c4cf3c17
Balint Csak <csak.balint@allwin.hu>
2024-02-28 00:33:09 +0100

    perf: shut down server if could not connect to redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,6 +3,14 @@ import Redis from 'ioredis';
 const redis = new Redis(
   parseInt(process.env.REDIS_PORT || '6379'),
   process.env.REDIS_HOST || 'localhost',
+  { maxRetriesPerRequest: 3 },
 );
+
+// fail fast, because we really want caching to work
+redis.on('error', (error) => {
+  // introduce better logging
+  console.error(error);
+  process.exit(1);
+});
 
 export default redis;
